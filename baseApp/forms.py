a7e74a376d1740853ddf5d9c1a2267686bf75227
harsharora05 <hararora12@gmail.com>
2024-06-12 18:56:37 +0530

    placeholder changed

--- baseApp/forms.py
+++ baseApp/forms.py
@@ -14,8 +14,8 @@ class RecipeForm(forms.ModelForm):
             'category' : forms.Select(attrs={'class':'form-control'}),
             'meal_prefrence' : forms.Select(attrs={'class':'form-control'}),
             'title' : forms.TextInput(attrs={'class':'form-control'}),
-            'ingredients' : forms.Textarea(attrs={'class':'form-control','rows':'4','placeholder':"Just give one space after every Ingredient. "}),
-            'steps' : forms.Textarea(attrs={'class':'form-control','rows':'4','placeholder':"Don't give extra gap between steps...Just give one space after every step. "}),
+            'ingredients' : forms.Textarea(attrs={'class':'form-control','rows':'4','placeholder':"Enter Ingredients"}),
+            'steps' : forms.Textarea(attrs={'class':'form-control','rows':'4','placeholder':"Enter Steps"}),
             'recipe_photo' : forms.FileInput(attrs={'class':'form-control'}),
         }
 
